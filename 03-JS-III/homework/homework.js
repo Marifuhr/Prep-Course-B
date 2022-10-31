afd8682484478e2782eaa462aa893357761eed39
Marifuhr <110607078+Marifuhr@users.noreply.github.com>
2022-10-31 16:45:37 -0300

    tarea complicada js03

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,47 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var suma = 0;
+    for (var i = 1; i < 11; i++){
+      suma = suma + i
+    }
+    console.log(suma);
+return suma;
 }
+
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+return array.filter( (e)=> e%2 === 0);
+
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map((num) => num ** 2);
+
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return array.reduce((prev, next) => prev + next);
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  console.log(num);
+  num += "";
+  console.log(typeof num);
+  console.log(num);
+  return num.length;
+
 }
   
   
